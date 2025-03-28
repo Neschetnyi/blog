e21f3c1431914d41db8b5afa76ee46a6ae4595e4
Neschetnyi <a.neschetnyi@gmail.com>
2025-03-28 20:04:41 +0500

    stable befor correcting redirection of routes

--- src/pages/SingleArticlePage.js
+++ src/pages/SingleArticlePage.js
@@ -1,27 +1,24 @@
 import { useParams, useNavigate } from "react-router-dom";
 import { deleteArticle, fetchArticle } from "../redux/SingleArticleSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import style from "./SingleArticle.module.scss";
 import { HeartOutlined } from "@ant-design/icons";
-import { Rate } from "antd";
-import { Tag } from "antd";
-import { Avatar } from "antd";
+import { Rate, Tag, Avatar, Modal } from "antd";
 import { fetchArticles } from "../redux/articlesListSlice";
 
 const SingleArticle = () => {
   const { slug } = useParams();
   const dispatch = useDispatch();
-  const navigate = useNavigate(); // Хук для навигации
+  const navigate = useNavigate();
   const articleData = useSelector((store) => store.article);
   const loading = useSelector((store) => store.article.loading);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     dispatch(fetchArticle(slug));
-  }, [slug, dispatch]);
-
-  console.log("articleData in SingleArticalPage is", articleData);
+  }, []);
 
   const {
     body,
@@ -40,37 +37,37 @@ const SingleArticle = () => {
     day: "numeric",
   });
 
-  console.log("article body is", body);
-  console.log("article tagList is", tagList);
-
   const handleEditClick = () => {
     const user = JSON.parse(localStorage.getItem("user"));
-    const username = user.username;
-
-    if (author.username === username) {
-      navigate(`/articles/${slug}/edit`, {
-        state: { articleData },
-      });
+    if (user?.username === author?.username) {
+      navigate(`/articles/${slug}/edit`, { state: { articleData } });
     } else {
       alert("forbidden");
     }
   };
 
-  const handleDeleteClick = () => {
+  const showDeleteModal = () => {
     const user = JSON.parse(localStorage.getItem("user"));
-    const username = user.username;
-    const token = user.token;
-    const data = {
-      token,
-      slug: slug,
-    };
-
-    if (author.username === username) {
-      dispatch(deleteArticle(data)).then(() => dispatch(fetchArticles()));
-      navigate("/");
+    if (user?.username === author?.username) {
+      setIsModalVisible(true);
     } else {
       alert("forbidden");
     }
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleDeleteConfirm = () => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user?.username === author?.username) {
+      const token = user.token;
+      const data = { token, slug };
+      dispatch(deleteArticle(data)).then(() => dispatch(fetchArticles()));
+      navigate("/");
+    }
+    setIsModalVisible(false);
   };
 
   return loading ? (
@@ -81,7 +78,6 @@ const SingleArticle = () => {
         <div className={style.title_container}>
           <div className={style.title}>
             {title}
-
             <Rate
               count={1}
               character={<HeartOutlined />}
@@ -90,44 +86,53 @@ const SingleArticle = () => {
             <span className={style.favoritesCount}>{favoritesCount}</span>
           </div>
           <div className={style.tag_container}>
-            {tagList && tagList.length > 0
-              ? tagList.map((tag, index) => (
-                  <Tag key={`${index}${tag}`}>{tag}</Tag>
-                ))
-              : null}
+            {tagList?.length > 0 &&
+              tagList.map((tag, index) => (
+                <Tag key={`${index}${tag}`}>{tag}</Tag>
+              ))}
           </div>
         </div>
         <div className={style.author_container}>
           <div className={style.author_data_container}>
-            <div className={style.autor_name}>{author.username}</div>
+            <div className={style.autor_name}>{author?.username}</div>
             <div className={style.date}>{creationDate}</div>
           </div>
-          <Avatar src={author.image} size={"large"} />
+          <Avatar src={author?.image} size={"large"} />
         </div>
       </div>
       <div className={style.text_container}>
         <div className={style.description_container}>
           <div className={style.description}>{description}</div>
-          {localStorage.getItem("user") ? (
+          {localStorage.getItem("user") && (
             <div className={style.button_container}>
               <button
                 className={`${style.button} ${style.delete}`}
-                onClick={handleDeleteClick}
+                onClick={showDeleteModal}
               >
                 Delete
               </button>
               <button
                 className={`${style.button} ${style.edit}`}
-                onClick={handleEditClick} // Обработчик клика
+                onClick={handleEditClick}
               >
                 Edit
               </button>
             </div>
-          ) : null}
+          )}
         </div>
-
         <ReactMarkdown>{markdown}</ReactMarkdown>
       </div>
+
+      <Modal
+        title="Confirm Deletion"
+        open={isModalVisible}
+        onOk={handleDeleteConfirm}
+        onCancel={handleCancel}
+        okText="Yes"
+        cancelText="No"
+      >
+        <p>Are you sure you want to delete this article?</p>
+      </Modal>
     </div>
   );
 };
